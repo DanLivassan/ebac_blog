a24cc11e2c6496637e400a7a5138f4092fcb93a6
DanLivassan <daniloxc@msn.com>
2022-01-22 00:19:55 -0300

    Start prject and app

---
+++ ebac_blog/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for ebac_blog project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ebac_blog.settings')
+
+application = get_wsgi_application()
