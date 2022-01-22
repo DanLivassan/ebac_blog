97a3b8f5d9b2a0df39a042c2d0657f81a3112d41
DanLivassan <daniloxc@msn.com>
2022-01-22 11:50:34 -0300

    Configure admin

--- blog/admin.py
+++ blog/admin.py
@@ -4,4 +4,11 @@ from django.contrib import admin
 
 from blog.models import Post
 
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'created_at', 'status')
+    list_filter = ('status',)
+    prepopulated_field = {'slug': ('title',)}
+
+
+admin.site.register(Post, PostAdmin)
