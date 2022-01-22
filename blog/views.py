9b80bceeb5fcfaaedc64b14be2cea3b591b7aad6
DanLivassan <daniloxc@msn.com>
2022-01-22 11:15:28 -0300

    create PostView

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import generic
 
-# Create your views here.
+
+class PostView(generic.View):
+    def get(self, request, *args, **kwargs):
+        return HttpResponse("Ola")
