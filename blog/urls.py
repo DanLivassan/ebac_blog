7b70094540579f0d08edf61cf731691a970c738f
DanLivassan <daniloxc@msn.com>
2022-01-24 20:06:23 -0300

    Finish post templates

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from blog.views import PostView
+from blog.views import PostView, PostDetailView
 
 urlpatterns = [
     path('', PostView.as_view(), name='home'),
+    path('<slug:slug>/', PostDetailView.as_view(), name='post-detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,17 @@
-from django.http import HttpResponse
 from django.views import generic
+from blog.models import Post
+from django.utils import timezone
 
 
-class PostView(generic.View):
-    def get(self, request, *args, **kwargs):
-        return HttpResponse("Ola")
+class PostView(generic.ListView):
+    queryset = Post.objects.filter(status=1).order_by('-created_at')
+    template_name = "index.html"
+
+
+class PostDetailView(generic.DetailView):
+    model = Post
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['now'] = timezone.now()
+        return context
